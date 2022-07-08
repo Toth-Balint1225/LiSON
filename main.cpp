db6b80877716d0bd2c4c7d2d801b94a2daf40d44
Tóth Bálint <toth.balint1225@gmail.com>
2022-07-08 10:25:09 +0200

    Added tests for float and int parsing

--- main.cpp
+++ main.cpp
@@ -10,11 +10,12 @@ using namespace lison;
 /**
  * Implementation of the LiSON interface
  */
+
+
 class MyObj : public LiSON
 {
 public:
-    // the class contains only one string, its LiSON equivalent is ('data')
-	std::list<std::string> data;
+	std::list<double> data;
 protected:
     /**
      * Implementation of the interpret function.
@@ -23,76 +24,12 @@ protected:
      */
     virtual void interpret(const Object& obj) override
     {
-		// this one still works
-		// auto interpreter = overload
-		// {
-		// 	[this] (const Tkn_Object& object)
-		// 	{
-		// 		auto innerInterpreter = overload
-		// 		{
-		// 			[] (const Tkn_Object& o) {},
-		// 			[this] (const Tkn_Literal& l)
-		// 			{
-		// 				data = l.value;
-		// 			},
-		// 			[this] (const Tkn_Error& e)
-		// 			{
-		// 				data = "ERROR";
-		// 			}
-		// 		};
-		// 		if (!object.value.empty())
-		// 			std::visit(innerInterpreter,object.value.front().token);
-		// 	},
-		// 	[] (const Tkn_Literal& l) {},
-		// 	[this] (const Tkn_Error& e)
-		// 	{
-		// 		data = "ERROR";
-		// 	}
-		// };
-		// std::visit(interpreter,obj.token);
-
-		// this is just another way of parsing the thing
-		// if (std::holds_alternative<Tkn_Object>(obj.token))
-		// {
-		// 	auto object = std::get<Tkn_Object>(obj.token);
-		// 	for (auto it : object.value)
-		// 		if (std::holds_alternative<Tkn_Literal>(it.token))
-		// 		{
-		// 			auto value = std::get<Tkn_Literal>(it.token);
-		// 			data.push_back(value.value);
-		// 		}
-		// }
-
-
 		// the shiny new api
 		obj.foreachObjectData([this](const Object& o)
 		{
-			data.push_back(o.expectLiteralData().value_or("ERROR"));
+			data.push_back(o.expectFloatData().value_or(0.0));
 		});
-		
     }
-
-	/*
-	std::optional<std::list<lison::Object>> object = obj.expectObjectData();
-
-	obj.foreachObjectData([&data](const lison::Object& obj) {
-		data.push_back(obj.expectLiteralData().value_or("ERROR"));
-	});
-
-	for (std::string it : data)
-		o_root.add(Object::fromString(it));
-
-	factory functions
-	static Object =>
-	fromString(std::string)
-	fromStringList(std::list<std::string>)
-	fromLiSON(const Lison&)
-	fromObjectList<T>(std::list<T>,std::function<Object(T)>)
-
-	std::list<Coord> cs;
-	Object::fromObjectList<Coord>(cs,[] (auto& c) {std::stringstream ss(); ss << "("<< c.x << "," << c.y << ")";
-		return Object::fromString(ss.str());}
-	*/
 
     /**
      * Implementation of the revert function.
@@ -100,33 +37,18 @@ protected:
      */
     virtual Object revert() const 
     {
-        // object for the ()
-        // Object o_root(Token{Tkn_Object{}});
-        // object for the literal
-        // set the data of the appropriate object
-
-
-		// auto& token = std::get<Tkn_Object>(o_root.token);
-		// for (auto it : data)
-		// {
-		// 	Object o_data(Token{Tkn_Literal{it}});
-		// 	token.value.push_back(o_data);
-		// }
-		
-		// new api candidate
-
 		Object o_root(Token{Tkn_Object{}});
+
 		for (auto it : data)
-			o_root.add(Object::fromString(it));
-        return o_root;
+			o_root.add(Object::fromFloat(it));
+
+		return o_root;
     }
 public:
     // some conveinence and debug functions
     MyObj()
     {}
 
-    MyObj(const std::string& _data)
-    {}
 
     void debug_print()
     {
